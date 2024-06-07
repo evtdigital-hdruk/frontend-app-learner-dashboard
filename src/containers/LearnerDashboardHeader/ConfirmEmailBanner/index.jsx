baaf3bc1475c1be1c070354f78cf9b113d1d51c9
Will <will@evt.digital>
2024-06-07 15:01:51 +0100

    Updated account activation modal

--- src/containers/LearnerDashboardHeader/ConfirmEmailBanner/index.jsx
+++ src/containers/LearnerDashboardHeader/ConfirmEmailBanner/index.jsx
@@ -1,11 +1,10 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   Button,
   Image,
   MarketingModal,
   ModalDialog,
-  PageBanner,
 } from '@edx/paragon';
 import { useIntl } from '@edx/frontend-platform/i18n';
 
@@ -18,56 +17,69 @@ export const ConfirmEmailBanner = () => {
   const {
     isNeeded,
     showConfirmModal,
-    showPageBanner,
-    closePageBanner,
     closeConfirmModal,
     openConfirmModalButtonClick,
-    userConfirmEmailButtonClick,
+    sendConfirmEmail,
   } = useConfirmEmailBannerData();
   const { formatMessage } = useIntl();
+  const openModalSendEmail = useRef();
+  const [waitForResend, setWaitForResend] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    if (isNeeded && !openModalSendEmail.current) {
+      openConfirmModalButtonClick();
+      openModalSendEmail.current = true;
+    }
+  }, [isNeeded, openConfirmModalButtonClick]);
+
+  useEffect(() => {
+    let timer;
+    if (waitForResend && timeLeft > 0) {
+      timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+    } else if (timeLeft === 0) {
+      setWaitForResend(false);
+    }
+    return () => clearTimeout(timer);
+  }, [waitForResend, timeLeft]);
+
+  const resendVerificationEmailClick = () => {
+    sendConfirmEmail();
+    setWaitForResend(true);
+    setTimeLeft(60);
+  };
 
   if (!isNeeded) { return null; }
 
   return (
-    <>
-      <PageBanner show={showPageBanner} dismissible onDismiss={closePageBanner}>
-        {formatMessage(messages.confirmEmailTextReminderBanner, {
-          confirmNowButton: (
-            <Button
-              className="confirm-email-now-button"
-              variant="link"
-              size="inline"
-              onClick={openConfirmModalButtonClick}
-            >
-              {formatMessage(messages.confirmNowButton)}
-            </Button>
-          ),
-        })}
-      </PageBanner>
-      <MarketingModal
-        title=""
-        isOpen={showConfirmModal}
-        onClose={closeConfirmModal}
-        hasCloseButton={false}
-        heroNode={(
-          <ModalDialog.Hero className="bg-gray-300">
-            <Image
-              className="m-auto"
-              src={confirmEmailSVG}
-              alt={formatMessage(messages.confirmEmailImageAlt)}
-            />
-          </ModalDialog.Hero>
-        )}
-        footerNode={(
-          <Button className="mx-auto my-3" variant="danger" onClick={userConfirmEmailButtonClick}>
-            {formatMessage(messages.verifiedConfirmEmailButton)}
-          </Button>
-        )}
-      >
-        <h1 className="text-center p-3">{formatMessage(messages.confirmEmailModalHeader)}</h1>
-        <p className="text-center">{formatMessage(messages.confirmEmailModalBody)}</p>
-      </MarketingModal>
-    </>
+    <MarketingModal
+      title=""
+      isOpen={showConfirmModal}
+      onClose={closeConfirmModal}
+      isBlocking
+      hasCloseButton={false}
+      heroNode={(
+        <ModalDialog.Hero className="bg-gray-300">
+          <Image
+            className="m-auto"
+            src={confirmEmailSVG}
+            alt={formatMessage(messages.confirmEmailImageAlt)}
+          />
+        </ModalDialog.Hero>
+      )}
+      footerNode={(
+        <Button className="mx-auto my-3" variant="danger" disabled={waitForResend} onClick={resendVerificationEmailClick}>
+          {
+            waitForResend
+              ? `${formatMessage(messages.resendConfirmEmailButton)} (${timeLeft}s)`
+              : formatMessage(messages.resendConfirmEmailButton)
+          }
+        </Button>
+      )}
+    >
+      <h1 className="text-center p-3">{formatMessage(messages.confirmEmailModalHeader)}</h1>
+      <p className="text-center">{formatMessage(messages.confirmEmailModalBody)}</p>
+    </MarketingModal>
   );
 };
 ConfirmEmailBanner.propTypes = {};
