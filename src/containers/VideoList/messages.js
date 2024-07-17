b1395938cec234718870e8ddd9b42198b2506b91
Will <will@evt.digital>
2024-07-17 09:45:46 +0100

    VideoList separate from CourseList

---
+++ src/containers/VideoList/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  myVideos: {
+    id: 'dashboard.myvideos',
+    defaultMessage: 'My Videos',
+    description: 'Video list heading',
+  },
+});
+
+export default messages;
