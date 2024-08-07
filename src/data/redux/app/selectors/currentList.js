c24098feb1c6441ca03d1db80b3ddbaf4631384a
Will <will@evt.digital>
2024-08-07 18:34:21 +0100

    Bug fix: visiblelist conflict that put courses on second page

--- src/data/redux/app/selectors/currentList.js
+++ src/data/redux/app/selectors/currentList.js
@@ -31,6 +31,7 @@ export const currentList = (allCourses, {
   sortBy,
   filters,
 }) => allCourses
+  .filter((course) => course.course.courseType !== 'video')
   .filter(module.courseFilterFn(filters))
   .sort(module.sortFn(transforms[sortBy], { reverse: sortBy === SortKeys.enrolled }));
 
