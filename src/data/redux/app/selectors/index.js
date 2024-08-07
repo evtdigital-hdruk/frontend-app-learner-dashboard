5a56f0cb9925f91c047103a95208acc707f46c0a
Will <will@evt.digital>
2024-08-07 13:43:57 +0100

    Add CurrentVideoList to reducers exported

--- src/data/redux/app/selectors/index.js
+++ src/data/redux/app/selectors/index.js
@@ -4,10 +4,12 @@ import simpleSelectors from './simpleSelectors';
 import appSelectors from './appSelectors';
 import courseCard from './courseCard';
 import currentList from './currentList';
+import currentVideoList from './currentVideoList';
 
 export default StrictDict({
   ...simpleSelectors,
   ...appSelectors,
   courseCard,
   currentList,
+  currentVideoList,
 });
