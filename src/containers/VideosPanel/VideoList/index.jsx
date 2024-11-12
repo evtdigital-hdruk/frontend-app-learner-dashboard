0a76ad8fc3755885f7ab3456e9572a93c4d87d71
Will <will@evt.digital>
2024-11-12 17:35:59 +0000

    VideosPanel component

---
+++ src/containers/VideosPanel/VideoList/index.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Pagination } from '@openedx/paragon';
+import {
+  ActiveVideoFilters,
+} from 'containers/VideoFilterControls';
+import CourseCard from 'containers/CourseCard';
+
+import { useIsCollapsed } from './hooks';
+
+export const VideoList = ({
+  filterOptions, setVideoPageNumber, numPages, showFilters, visibleVideoList,
+}) => {
+  const isCollapsed = useIsCollapsed();
+  return (
+    <>
+      {showFilters && (
+        <div id="course-list-active-filters-container">
+          <ActiveVideoFilters {...filterOptions} />
+        </div>
+      )}
+      <div className="d-flex flex-column flex-grow-1 container">
+        <div className="row">
+          {visibleVideoList.map(({ cardId }) => (
+            <CourseCard key={cardId} cardId={cardId} />
+          ))}
+        </div>
+        {numPages > 1 && (
+          <Pagination
+            variant={isCollapsed ? 'reduced' : 'secondary'}
+            paginationLabel="Course List"
+            className="mx-auto mb-2"
+            pageCount={numPages}
+            onPageSelect={setVideoPageNumber}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+VideoList.propTypes = {
+  showFilters: PropTypes.bool.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  visibleVideoList: PropTypes.arrayOf(PropTypes.object).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  filterOptions: PropTypes.object.isRequired,
+  numPages: PropTypes.number.isRequired,
+  setVideoPageNumber: PropTypes.func.isRequired,
+};
+
+export default VideoList;
