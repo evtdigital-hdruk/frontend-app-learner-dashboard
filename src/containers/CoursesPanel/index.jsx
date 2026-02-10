3809dda9790a0d1f470f4666ca833200ee70d77f
Will <49617560+WillEVTDigital@users.noreply.github.com>
2026-02-10 17:51:30 +0000

    Introduces CourseListHeadingSlot plugin slot
    Moves the course list heading and filter controls into a new plugin slot.

--- src/containers/CoursesPanel/index.jsx
+++ src/containers/CoursesPanel/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 
-import { useIntl } from '@edx/frontend-platform/i18n';
-
 import { reduxHooks } from 'hooks';
-import {
-  CourseFilterControls,
-} from 'containers/CourseFilterControls';
+import CourseListHeadingSlot from 'plugin-slots/CourseListHeadingSlot';
 import CourseListSlot from 'plugin-slots/CourseListSlot';
 import NoCoursesViewSlot from 'plugin-slots/NoCoursesViewSlot';
 
 import { useCourseListData } from './hooks';
-
-import messages from './messages';
 
 import './index.scss';
 
@@ -21,17 +15,11 @@ import './index.scss';
  * @returns List of courses as CourseCards or empty state
 */
 export const CoursesPanel = () => {
-  const { formatMessage } = useIntl();
   const hasCourses = reduxHooks.useHasCourses();
   const courseListData = useCourseListData();
   return (
     <div className="course-list-container">
-      <div className="course-list-heading-container">
-        <h2 className="course-list-title">{formatMessage(messages.myCourses)}</h2>
-        <div className="course-filter-controls-container">
-          <CourseFilterControls {...courseListData.filterOptions} />
-        </div>
-      </div>
+      <CourseListHeadingSlot filterOptions={courseListData.filterOptions} />
       {hasCourses ? <CourseListSlot courseListData={courseListData} /> : <NoCoursesViewSlot />}
     </div>
   );
